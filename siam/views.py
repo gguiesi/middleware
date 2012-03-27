7212a0cac23a7f70cf9f51c05bc9e8c560f354f8
Geraldo Guiesi <geraldo.guiesi@gmail.com>
2012-03-27 11:27:54 -0300

    inclusão de comunicação com soap e impressão do json na tela

--- siam/views.py
+++ siam/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+from suds.client import Client
+import json
+
+def index(request):
+    url = 'http://labimedsys.no-ip.org:8081/wssiam/ws/service?wsdl'
+    client = Client(url)
+    result = client.service.getEnv('siam', 'siam1234', '101')
+    return HttpResponse(json.dumps(json.loads(result), sort_keys=True, indent=4))
--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from siam.views import index
+# Uncomment the next two lines to enable the admin:
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^siam/$', index),
 )
